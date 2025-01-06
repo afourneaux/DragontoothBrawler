9bc5ebadc3acb8312e73e88d30d5c5053c5b6bef
Alex Fourneaux <Alex@fourneaux.ca>
2025-01-05 19:49:29 -0500

    Reset from menu on disconnect

--- Scripts/PlayerData.gd
+++ Scripts/PlayerData.gd
@@ -5,23 +5,32 @@ signal on_update_player_data
 enum Field {NAME, CHARACTER, READY}
 
 # Basic info
-var player_name = "":
+var _player_name = ""
+var player_name:
 	set(value):
-		if value != player_name:
-			player_name = value
+		if value != _player_name:
+			_player_name = value
 			on_update_player_data.emit(package_data())
-var selected_character = Globals.Character.NONE:
+	get:
+		return _player_name
+var _selected_character = Globals.Character.NONE
+var selected_character:
 	set(value):
-		if value != selected_character:
-			selected_character = value
+		if value != _selected_character:
+			_selected_character = value
 			on_update_player_data.emit(package_data())
+	get:
+		return _selected_character
 
 # Lobby info
-var is_ready = false:
+var _is_ready = false
+var is_ready:
 	set(value):
-		if value != is_ready:
-			is_ready = value
+		if value != _is_ready:
+			_is_ready = value
 			on_update_player_data.emit(package_data())
+	get:
+		return _is_ready
 
 func package_data():
 	return {
@@ -29,3 +38,8 @@ func package_data():
 		Field.CHARACTER: selected_character,
 		Field.READY: is_ready
 	}
+
+func reset():
+	_player_name = ""
+	_selected_character = Globals.Character.NONE
+	_is_ready = false
--- Scripts/Server.gd
+++ Scripts/Server.gd
@@ -2,6 +2,8 @@ extends Node
 
 signal on_data_changed
 
+var peer
+
 var _player_data = {}:
 	set(value):
 		_player_data = value
@@ -13,6 +15,14 @@ func _ready():
 	multiplayer.peer_disconnected.connect(_on_peer_disconnected)
 	multiplayer.connected_to_server.connect(_register_new_player)
 	PlayerData.on_update_player_data.connect(_client_send_player_data)
+
+func connect_peer():
+	peer = ENetMultiplayerPeer.new()
+
+func disconnect_peer():
+	_player_data = {}
+	peer.close()
+	multiplayer.multiplayer_peer = null
 
 func _on_peer_disconnected(id):
 	_player_data.erase(id)
