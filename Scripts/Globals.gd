23736248b75a749ad31e6264dd11a660ec7d0509
Alex Fourneaux <Alex@fourneaux.ca>
2026-04-12 17:44:26 -0400

    Upgrade to Godot 4.6

--- Scripts/Globals.gd
+++ Scripts/Globals.gd
@@ -1,5 +1,8 @@
 extends Node
 
+# TODO investigate migrating globals to other object types
+# https://docs.godotengine.org/en/4.4/tutorials/best_practices/node_alternatives.html
+
 const SCENE_LOBBY_MENU = "LobbyMenu"
 const SCENE_MAIN_MENU = "MainMenu"
 const SCENE_CHARACTER_MENU = "CharacterSelectMenu"
